b512f78512a771574e5d026301453142ede455db
Wilson Reis <wilsonreisccp@gmail.com>
2023-02-11 16:05:14 -0300

    chore: test cors front

--- src/pages/api/dizimistas.ts
+++ src/pages/api/dizimistas.ts
@@ -1,9 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
+import Cors from 'cors';
 
 const prisma = new PrismaClient();
 
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+);
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  // Run cors
+  await cors(req, res);
   if (req.method === "GET") {
     // get all dizimistas
     const dizimistas = await prisma.dizimista.findMany({
@@ -12,3 +24,16 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     res.json(dizimistas);
   }
 };
+
+
+function initMiddleware(middleware) {
+  return (req, res) =>
+    new Promise((resolve, reject) => {
+      middleware(req, res, (result) => {
+        if (result instanceof Error) {
+          return reject(result)
+        }
+        return resolve(result)
+      })
+    })
+}
